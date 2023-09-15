e9ce6dfff3452e05ce7da8b3fdbf3c45a8e7659b
AbdullahAbuHaltam <abuhaltamabdullah@gmail.com>
2023-09-15 14:16:46 +0300

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[UserService]
 })
 export class AppComponent {
-  oddNumbers:number[]=[];
-  evenNumbers:number[]=[];
 
-
-  onIntervalFired(firedNumber:number){
-    if(firedNumber%2==0){
-      this.evenNumbers.push(firedNumber);
-    } else{
-      this.oddNumbers.push(firedNumber);
-    }
-  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { CounterService } from './counter.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent
+    ActiveUsersComponent,
+    InactiveUsersComponent,
+   
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
